58a7a399f5604c0e1b56f722d1ad52f3b038b6e2
1076723645 <1076723645@qq.com>
2017-06-10 22:29:31 +0800

    6.10

--- 15_game/15_game/code/common_form_test.js
+++ 15_game/15_game/code/common_form_test.js
@@ -1,17 +1,17 @@
 // 全局变量a和b，分别获取用户框和密码框的value值
-var a = document.getElementsByTagName("input")[0].value;
-var b = document.getElementsByTagName("input")[1].value;
+var a = document.getElementsByTagName("input")[0].val();
+var b = document.getElementsByTagName("input")[1].val();
 
 
 //若输入框为空，阻止表单的提交
 function submitTest() {
    
-    if (a=='') { //用户框value值为空
+    if (a=="") { //用户框value值为空
         document.getElementById("remind_1").innerHTML = "请输入用户名！";
         document.getElementById("change_margin_1").style.marginBottom = 1 + "px";
         return false;
     }
-	if (b=='') { //密码框value值为空
+	if (b=="") { //密码框value值为空
         document.getElementById("remind_2").innerHTML = "请输入密码！";
         document.getElementById("change_margin_2").style.marginBottom = 1 + "px";
         document.getElementById("change_margin_3").style.marginTop = 2 + "px";
